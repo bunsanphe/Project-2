e0c94bff82795bcd4bb7f86d7eff3b3aa2af5904
Heather Empson <hrempson@gmail.com>
2020-07-09 21:51:41 -0700

    db schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO songs (artist, song_title)
+VALUES ("The Head and the Heart", "Honeybee");
+INSERT INTO songs (artist, song_title)
+VALUES ("Katy Perry", "Firework");
+INSERT INTO songs (artist, song_title)
+VALUES ("Pink Floyd", "Time");
+
+INSERT INTO playlist (username_id, playlist_name)
+VALUES (1, "New Playlist");
+
+INSERT INTO user (username)
+VALUES ("user1@gmail.com");
+
+
