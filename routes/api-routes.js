13f2d5c73d8043f1d687a69ec28466792a6ab0cb
Heather Empson <hrempson@gmail.com>
2020-07-12 15:03:21 -0700

    added handlebars for each playlist

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -53,6 +53,26 @@ module.exports = function(app) {
 };
 
 // Create route that displays the list of playlists
+app.get("/api/playlist", (req, res) => {
+  db.Playlist.findAll({})
+    .then( (dbPlaylist) => {
+      res.json(dbPlaylist);
+    });
+});
+
 // Create route to add song to playlist
+
 // Create route to delete song(s) from playlist
+
 // Create route to delete playlist
+app.delete("/api/playlist/:id", (req,res) => {
+  db.Playlist.destroy(req.body,
+  {
+    where: {
+      id: req.body.id
+    }
+  })
+  .then( dbPlaylist => {
+    res.json(dbPlaylist)
+  })
+})
--- server.js
+++ server.js
@@ -5,8 +5,6 @@ const exphbs = require("express-handlebars");
 // Requiring passport as we've configured it
 const passport = require("./config/passport");
 
-const appController = require("./controllers/app_controllers");
-
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
 const db = require("./models");
@@ -26,8 +24,6 @@ app.use(passport.session());
 //handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
-app.use(appController);
 
 // Requiring our routes
 require("./routes/html-routes.js")(app);
