2a90134a47b58e5a69ae9b26aece743e8f61b24c
Heather Empson <hrempson@gmail.com>
2020-07-12 17:42:36 -0700

    removed html tags from handlebars

--- models/index.js
+++ models/index.js
@@ -1,37 +1,49 @@
-'use strict';
+"use strict";
 
-var fs = require('fs');
-var path = require('path');
-var Sequelize = require('sequelize');
-var basename  = path.basename(module.filename);
-var envProc   = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config.json')[envProc];
-var db        = {};
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
+const basename = path.basename(module.filename);
+const envProc = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../config/config.json")[envProc];
+const db = {};
 
 const env = require("node-env-file");
 const envPath = path.join(__dirname, "../.env");
 
 try {
-  if(fs.existsSync(envPath)) env(envPath);
-} catch(err) {console.log("\nMissing .env file. Complete first time set up and then restart.");}
-
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
-  var sequelize = new Sequelize(config.database, config.username, process.env.DB_PASSWORD, config);
+  if (fs.existsSync(envPath)) {
+    env(envPath);
+  }
+} catch (err) {
+  console.log(
+    "\nMissing .env file. Complete first time set up and then restart."
+  );
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(function(file) {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+if (config.use_env_variable) {
+  const sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+  const sequelize = new Sequelize(
+    config.database,
+    config.username,
+    process.env.DB_PASSWORD,
+    config
+  );
+}
+
+fs.readdirSync(__dirname)
+  .filter(file => {
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
   })
-  .forEach(function(file) {
-    var model = sequelize['import'](path.join(__dirname, file));
+  .forEach(file => {
+    const model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(function(modelName) {
+Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
