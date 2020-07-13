d4614862c99e2d6d31d7abd247a5dffcded1e718
Heng <bunsanphe@gmail.com>
2020-07-12 19:36:52 -0700

    changed all const back to var in index.js.  Added playlist api routes

---
+++ routes/playlist-routes.js
@@ -0,0 +1,60 @@
+const db = require("../models");
+const passport = require("../config/passport");
+
+module.exports = function(app){
+
+// Create route that displays the list of all playlists
+app.get("/api/playlist", (req, res) => {
+    var query = {};
+    if (req.query.user_id) {
+        query.UserId = req.query.user_id;
+    }
+    db.Playlist.findAll({}).then(dbPlaylist => {
+      res.json(dbPlaylist);
+    });
+  });
+
+//create playlist
+  app.post("/api/playlist", (req, res) => {
+      db.Playlist.create(req.body).then( dbPlaylist => {
+          res.json(dbPlaylist);
+      })
+  })
+
+  // route to select one playlist
+  app.get("/api/playlist/:id", (req, res) => {
+      db.Playlist.findOne({
+          where: {
+              id: req.params.id
+          }
+      }).then( dbPlaylist => {
+          res.json(dbPlaylist)
+      })
+  })
+
+  // route to update playlist
+  app.put("/api/playlist", (req, res) => {
+      db.Playlist.update(
+          req.boy,
+          {where : {
+              id: req.body.id
+          }}
+      ).then( dbPlaylist => {
+          res.json(dbPlaylist)
+      })
+  })
+  // Create route to add song to playlist
+  
+  // Create route to delete song(s) from playlist
+  
+  // Create route to delete playlist
+  app.delete("/api/playlist/:id", (req, res) => {
+    db.Playlist.destroy(req.body, {
+      where: {
+        id: req.body.id
+      }
+    }).then(dbPlaylist => {
+      res.json(dbPlaylist);
+    });
+  });
+}
