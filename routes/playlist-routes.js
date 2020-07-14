57656cc61d743c20826c66278ad36a1ee15c48e0
Heng <bunsanphe@gmail.com>
2020-07-13 17:26:54 -0700

    saved playlistapi route for lint

--- routes/playlist-routes.js
+++ routes/playlist-routes.js
@@ -1,52 +1,51 @@
 const db = require("../models");
-const passport = require("../config/passport");
+// const passport = require("../config/passport");
+// const playlist = require("../models/playlist");
 
-module.exports = function(app){
-
-// Create route that displays the list of all playlists
-app.get("/api/playlist", (req, res) => {
-    var query = {};
+module.exports = function(app) {
+  // Create route that displays the list of all playlists
+  app.get("/api/playlist", (req, res) => {
+    const query = {};
     if (req.query.user_id) {
-        query.UserId = req.query.user_id;
+      query.UserId = req.query.user_id;
     }
     db.Playlist.findAll({}).then(dbPlaylist => {
       res.json(dbPlaylist);
     });
   });
 
-//create playlist
+  //create playlist
   app.post("/api/playlist", (req, res) => {
-      db.Playlist.create(req.body).then( dbPlaylist => {
-          res.json(dbPlaylist);
-      })
-  })
+    db.Playlist.create(req.body).then(dbPlaylist => {
+      res.json(dbPlaylist);
+    });
+  });
 
   // route to select one playlist
   app.get("/api/playlist/:id", (req, res) => {
-      db.Playlist.findOne({
-          where: {
-              id: req.params.id
-          }
-      }).then( dbPlaylist => {
-          res.json(dbPlaylist)
-      })
-  })
+    db.Playlist.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(dbPlaylist => {
+      res.json(dbPlaylist);
+    });
+  });
 
   // route to update playlist
   app.put("/api/playlist", (req, res) => {
-      db.Playlist.update(
-          req.boy,
-          {where : {
-              id: req.body.id
-          }}
-      ).then( dbPlaylist => {
-          res.json(dbPlaylist)
-      })
-  })
+    db.Playlist.update(req.boy, {
+      where: {
+        id: req.body.id
+      }
+    }).then(dbPlaylist => {
+      res.json(dbPlaylist);
+    });
+  });
   // Create route to add song to playlist
-  
+
   // Create route to delete song(s) from playlist
-  
+
   // Create route to delete playlist
   app.delete("/api/playlist/:id", (req, res) => {
     db.Playlist.destroy(req.body, {
@@ -57,4 +56,4 @@ app.get("/api/playlist", (req, res) => {
       res.json(dbPlaylist);
     });
   });
-}
+};
