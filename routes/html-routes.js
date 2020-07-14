c66ea75ac9d95ff0335cb4264c63c3334ed6a0ed
anthnsm <anthony.smith353@gmail.com>
2020-07-13 17:56:18 -0700

    Added handlebar files, added a couple test lines

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -10,7 +10,7 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.render("index");
   });
 
   app.get("/login", (req, res) => {
@@ -18,7 +18,7 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+    res.render("login");
   });
 
   app.get("/ajaxTest", (req, res) => {
