0cc6a5ed110fdb32e242fdcbe39c8e4ff6dc84e8
anthnsm <anthony.smith353@gmail.com>
2020-07-14 18:37:42 -0700

    Please the Travis Gods

--- routes/playlist-routes.js
+++ routes/playlist-routes.js
@@ -11,7 +11,7 @@ module.exports = function(app) {
     }
     db.Playlist.findAll({}).then(dbPlaylist => {
       console.log(dbPlaylist);
-      res.render("members", {playlist: dbPlaylist});
+      res.render("members", { playlist: dbPlaylist });
     });
   });
 
